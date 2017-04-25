d6ed8c49526463c8d1a763e1bd814df3bb0c715f
james sewell <jamesjosephsewell@gmail.com>
2017-04-25 18:14:25 -0500

    added a basic header

--- src/scripts/views/landingPage.js
+++ src/scripts/views/landingPage.js
@@ -30,9 +30,38 @@ const LandingPage = React.createClass({
 			
 			return (
 
-		 		<div className='blue-grey lighten-3'>
+		 		<div className='deep-orange darken-4'>
 
 		 			<div className='row'>
+
+		 				<div className='col s12 m6 offset-m3'>
+
+		 					<h2 className="center-align white-text">JAMES SEWELL</h2>
+		 					<h5 className="center-align white-text ">Front-end Engineer - Houston, Tx</h5>
+
+		 				</div>
+
+		 			</div>
+
+		 			<div className='row'>
+
+		 				<div className='col s12 m6'>
+		 		
+		 					<div className='card blue-grey darken-4'>
+
+				 				<div className="card-content white-text flow-text">
+
+				 					<h3 className='blue-grey darken-3 card-title'><i className="medium material-icons">insert_chart</i>JAMES</h3>
+				 				
+				 					<p>lorem ipsum dom ado aid oaa oado adoia oadi adfado dsfd fdfso adfdso ad dooa addo a adddo adooa ado adoo adoo adaod</p>
+
+				 					<a className="waves-effect waves-light btn deep-orange"><i className="material-icons left">cloud</i>button</a>
+
+				 				</div>
+
+		 					</div>
+
+		 				</div>
 
 		 				<div className='col s12 m6'>
 		 		
